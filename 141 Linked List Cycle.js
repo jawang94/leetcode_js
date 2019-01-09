ab50177211a5582fdb3b28462edd01c35743b5fb
Jason Wang <wang.jason.010@gmail.com>
2019-01-08 21:08:52 -0800

    updated Linked List Cycle

--- 141 Linked List Cycle.js
+++ 141 Linked List Cycle.js
@@ -10,65 +10,15 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-// var hasCycle = function(head) {
-//     if(head === null || head.next === null){
-//         return false;
-//     }
-
-//     var faster = head.next;
-//     var slower = head;
-
-//     while(faster && slower){
-//         if(faster.val === slower.val){
-//             return true;
-//         }
-//         faster = faster.next;
-
-//         if(faster === null){
-//             return false;
-//         } else {
-//             faster = faster.next;
-//         }
-
-//         slower = slower.next;
-//     }
-
-//     return false;
-// };
-
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-
-/**
- * @param {ListNode} head
- * @return {boolean}
- */
 var hasCycle = function(head) {
-  if (head === null) {
-    return false;
+  if (head === null) return false;
+  else if (head === []) return head;
+  let walker = head;
+  let runner = head.next;
+  while (walker != null && runner != null && runner.next != null) {
+    if (walker === runner) return true;
+    walker = walker.next;
+    runner = runner.next.next;
   }
-
-  var node1 = head;
-  var node2 = head;
-  node2 = node2.next;
-
-  while (node1 !== null && node2 !== null) {
-    if (node1.val === node2.val) {
-      return true;
-    }
-
-    node1 = node1.next;
-    node2 = node2.next;
-
-    if (node2 !== null) {
-      node2 = node2.next;
-    }
-  }
-
   return false;
 };
