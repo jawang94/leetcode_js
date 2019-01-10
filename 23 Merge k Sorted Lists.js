e4aff453ba5707834664c0742e10c85ccdebd2b4
Jason Wang <wang.jason.010@gmail.com>
2019-01-10 12:19:37 -0800

    23 merge K sorted lists. two solutions

--- 23 Merge k Sorted Lists.js
+++ 23 Merge k Sorted Lists.js
@@ -18,50 +18,59 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-var mergeKLists = function(lists) {
-  function merge(beg, end, lists) {
-    if (beg > end) {
-      return [];
-    }
-
-    if (beg === end) {
-      return lists[beg];
-    }
-
-    var mid = beg + Math.floor((end - beg) / 2);
-    var left = merge(beg, mid, lists);
-    var right = merge(mid + 1, end, lists);
-
-    return mergeTwoLists(left, right);
+// Efficient solution. O(NlogM) and around 80ms
+var mergeKLists = function(lists, low = 0, high = lists.length - 1) {
+  if (lists.length === 0) return null;
+  if (low === high) {
+    return lists[low];
   }
 
-  function mergeTwoLists(list1, list2) {
-    var head = new ListNode(0);
-    var tmp = head;
+  let mid = Math.floor((low + high) / 2);
+  let left = mergeKLists(lists, low, mid);
+  let right = mergeKLists(lists, mid + 1, high);
 
-    while (list1 && list2) {
-      if (list1.val < list2.val) {
-        tmp.next = list1;
-        list1 = list1.next;
-      } else {
-        tmp.next = list2;
-        list2 = list2.next;
-      }
-
-      tmp = tmp.next;
-    }
-
-    if (list1) {
-      tmp.next = list1;
-    } else if (list2) {
-      tmp.next = list2;
-    }
-
-    tmp = head.next;
-    head.next = null;
-
-    return tmp;
-  }
-
-  return merge(0, lists.length - 1, lists);
+  return merge(left, right);
 };
+
+const merge = (l1, l2) => {
+  if (l1 === null) {
+    return l2;
+  }
+  if (l2 === null) {
+    return l1;
+  }
+  if (l1.val < l2.val) {
+    l1.next = merge(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = merge(l1, l2.next);
+    return l2;
+  }
+};
+
+// Slow solution - O(N*M) and about 340ms
+// var mergeKLists = function(lists) {
+//   if (lists.length === 0) return null;
+
+//   for (var i = 1; i < lists.length; i++) {
+//     lists[0] = merge(lists[0], lists[i]);
+//   }
+
+//   return lists[0];
+// };
+
+// const merge = (l1, l2) => {
+//   if (l1 === null) {
+//     return l2;
+//   }
+//   if (l2 === null) {
+//     return l1;
+//   }
+//   if (l1.val < l2.val) {
+//     l1.next = merge(l1.next, l2);
+//     return l1;
+//   } else {
+//     l2.next = merge(l1, l2.next);
+//     return l2;
+//   }
+// };
