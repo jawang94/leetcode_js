b7ac2fa64a155a7421186ec00bee39b54286cdc1
Jason Wang <wang.jason.010@gmail.com>
2019-01-24 13:11:30 -0800

    #53 quadratic brute force

--- 53 Maximum Subarray.js
+++ 53 Maximum Subarray.js
@@ -17,3 +17,23 @@ var maxSubArray = function(nums) {
 
   return maxSum;
 };
+
+// Terrible n^2 solution
+var maxSubArray = function(nums) {
+  const n = nums.length;
+  let max = nums[0];
+  if (n === 2) {
+    max = Math.max(max, nums[1], max + nums[1]);
+    return max;
+  }
+
+  for (var i = 0; i < n; i++) {
+    if (nums[i] > max) max = nums[i];
+    let sum = nums[i];
+    for (var j = i + 1; j < n; j++) {
+      sum += nums[j];
+      if (sum > max) max = sum;
+    }
+  }
+  return max;
+};
