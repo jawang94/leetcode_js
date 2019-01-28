8d15dbd654c306ad5f7fc1c21df89e11d8674ec8
Jason Wang <wang.jason.010@gmail.com>
2019-01-27 22:26:00 -0800

    #139 memoization 100%

--- 139 Word Break.js
+++ 139 Word Break.js
@@ -26,3 +26,29 @@ var wordBreak = function(s, wordDict) {
 
   return possible[s.length] === true;
 };
+
+// Faster than 100%, using memoization
+var wordBreak = function(s, wordDict) {
+  if (wordDict.length === 0) return false;
+  if (wordDict.length === 1) return s === wordDict[0];
+
+  let queue = [""];
+  let memo = new Map();
+
+  while (queue.length > 0) {
+    const val = queue.shift();
+
+    for (let word of wordDict) {
+      const searchWord = `${val}${word}`;
+      const startsWith = s.indexOf(searchWord) === 0;
+
+      if (searchWord === s) return true;
+      else if (!memo.has(searchWord) && startsWith) {
+        memo.set(searchWord, true);
+        queue.push(searchWord);
+      }
+    }
+  }
+
+  return false;
+};
