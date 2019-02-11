1b96e25ff88f2dcc701465462143be78af8646ae
Jason Wang <wang.jason.010@gmail.com>
2019-02-11 12:26:01 -0800

    140 word break II refactor

--- 140 Word Break II.js
+++ 140 Word Break II.js
@@ -6,48 +6,37 @@
  *   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
  * @return {string[]}
  */
-var wordBreak = function(s, wordDict) {
-  var result = [];
-  var solutions = [];
-  var len = s.length;
-  var possible = [];
+const wordBreak = (s, wordDict) => {
+  const len = s.length;
+  let result = [],
+    solutions = [],
+    possible = [];
 
-  for (var i = 0; i <= s.length; i++) {
-    possible.push(true);
-  }
+  for (let i = 0; i <= len; i++) possible.push(true);
 
-  getAllSolutions(0, s, wordDict, result, solutions, possible);
+  recurse(0, s, wordDict, result, solutions, possible);
   return solutions;
 };
 
-function getAllSolutions(start, s, wordDict, result, solutions, possible) {
+const recurse = (start, s, wordDict, result, solutions, possible) => {
   if (start === s.length) {
     solutions.push(result.join(" ")); // remove the last space
     return;
   }
 
   // loop through string from i to s.length
-  for (var i = start; i < s.length; i++) {
-    var piece = s.substring(start, i + 1);
+  for (let i = start; i < s.length; i++) {
+    let piece = s.substring(start, i + 1);
 
     // possible is to mark step whether i+1 to s.length have any possible words
-    if (wordDict.has(piece) && possible[i + 1]) {
+    if (wordDict.includes(piece) && possible[i + 1]) {
       // eliminate unnecessary search
+      let beforeChange = solutions.length;
+
       result.push(piece);
-      var beforeChange = solutions.length;
-      getAllSolutions(i + 1, s, wordDict, result, solutions, possible);
-      if (solutions.length === beforeChange) {
-        possible[i + 1] = false;
-      }
+      recurse(i + 1, s, wordDict, result, solutions, possible);
+      if (solutions.length === beforeChange) possible[i + 1] = false;
       result.pop();
     }
   }
-}
-
-var dict = new Set();
-dict.add("leet");
-dict.add("code");
-dict.add("cod");
-dict.add("de");
-
-wordBreak("leetcode", dict);
+};
