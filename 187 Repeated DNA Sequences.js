584c1e0666149134fd5281fc808c84d8927ebff2
Jason Wang <wang.jason.010@gmail.com>
2019-02-25 13:55:28 -0800

    repeated DNA sequences 2 solutions

--- 187 Repeated DNA Sequences.js
+++ 187 Repeated DNA Sequences.js
@@ -31,3 +31,52 @@ var findRepeatedDnaSequences = function(s) {
 
   return result;
 };
+
+// 84ms faster than 96.50% of solutions
+var findRepeatedDnaSequences = function(s) {
+  let store = new Set(),
+    result = new Set();
+  for (let i = 0; i < s.length - 9; i++) {
+    const str = s.substring(i, i + 10);
+    if (store.has(str)) {
+      result.add(str);
+    } else {
+      store.add(str);
+    }
+  }
+  return Array.from(result);
+};
+
+// Should work but doesn't work (not sure why?)
+// var findRepeatedDnaSequences = function(s) {
+//     if (s.length < 1) return [];
+//     let result = [];
+//     let sequence = "";
+
+//     for (var i = 0; i < s.length - 9; i++) {
+//         for (var j = i; j < s.length; j++) {
+//             if (sequence.length === 10) {
+//                 let left = s.slice(0, i);
+//                 let right = s.slice(j);
+//                 let inputSlice;
+
+//                 if (left.length > 9 && right.length > 9) inputSlice = left.concat(right);
+//                 else if (left.length > 9 && right.length < 10) inputSlice = left;
+//                 else inputSlice = right;
+//                 console.log(inputSlice, sequence);
+//                 let possible = DnaSequencer(inputSlice, sequence);
+//                 if (possible != -1 && result.indexOf(possible) === -1) result.push(possible);
+//             } else sequence += s[j];
+//         }
+//         sequence = "";
+//     }
+
+//     return result;
+// };
+
+// const DnaSequencer = (input, sequence) => {
+//     if (input.indexOf(sequence) > -1) {
+//         return sequence;
+//     }
+//     return -1;
+// }
