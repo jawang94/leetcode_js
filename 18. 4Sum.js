f26888dc321b38ec649d49d97c77e082a42e10cf
Jason Wang <wang.jason.010@gmail.com>
2019-02-26 23:58:00 -0800

    4sum stable 88ms solution

--- 18. 4Sum.js
+++ 18. 4Sum.js
@@ -41,3 +41,50 @@ var fourSum = function(nums, target) {
   }
   return results;
 };
+
+// Solution 2 (more stable than solution 1). Consistently 92ms, as low as 88ms faster than 97.45%
+var fourSum = function(nums, target) {
+  var ret = [];
+
+  if (nums.length == 0) return ret;
+
+  nums.sort(function(a, b) {
+    return a - b;
+  });
+
+  for (var i = 0; i < nums.length; i++) {
+    var target2 = target - nums[i];
+
+    for (var j = i + 1; j < nums.length; j++) {
+      var target3 = target2 - nums[j];
+
+      var front = j + 1;
+      var back = nums.length - 1;
+
+      while (front < back) {
+        var sum = nums[front] + nums[back];
+
+        if (sum < target3) front++;
+        else if (sum > target3) back--;
+        else {
+          var temp = new Array(4);
+          temp[0] = nums[i];
+          temp[1] = nums[j];
+          temp[2] = nums[front];
+          temp[3] = nums[back];
+          ret.push(temp);
+
+          while (front < back && nums[front] === temp[2]) front++;
+
+          while (front < back && nums[back] === temp[3]) back--;
+        }
+      }
+
+      while (j + 1 < nums.length && nums[j + 1] === nums[j]) ++j;
+    }
+
+    while (i + 1 < nums.length && nums[i + 1] === nums[i]) ++i;
+  }
+
+  return ret;
+};
