5816069c4be4f5e4a3f89be748f48fce605f459b
Jason Wang <wang.jason.010@gmail.com>
2019-03-10 11:48:50 -0700

    189 rotate array concise solution

--- 189 Rotate Array.js
+++ 189 Rotate Array.js
@@ -24,3 +24,12 @@ var rotate = function(nums, k) {
   nums.reverseFromLToR(0, k - 1);
   nums.reverseFromLToR(k, nums.length - 1);
 };
+
+// Concise solution 96ms faster than ~54.31% & space 35.3mb less than ~45%
+var rotate = function(nums, k) {
+  while (k > 0) {
+    let temp = nums.pop();
+    nums.unshift(temp);
+    k--;
+  }
+};
