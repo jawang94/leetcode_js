bb2febdedd0ca2cc5c8aba2e18f6007bbe34dc47
Jason Wang <wang.jason.010@gmail.com>
2019-03-20 15:38:16 -0700

    drop nth node from end of SLL 85% speed and 100% memory efficiency

--- 19 Remove Nth Node From End of List.js
+++ 19 Remove Nth Node From End of List.js
@@ -36,3 +36,36 @@ var removeNthFromEnd = function(head, n) {
 
   return dummy.next;
 };
+
+// One pass, 64ms faster than ~85.22% and 33.8mb less than 100%
+var removeNthFromEnd = function(head, n) {
+  if (head === null) return head;
+
+  let nodeMap = new Map();
+  let current = head;
+  let count = 0;
+
+  while (current != null) {
+    nodeMap.set(count, current);
+    current = current.next;
+    count += 1;
+  }
+
+  let nthNode = count - n;
+
+  if (nodeMap.has(nthNode)) {
+    let target = nodeMap.get(nthNode);
+
+    if (nthNode === 0) {
+      let next = target.next;
+      target.next = null;
+      return next;
+    } else {
+      let prev = nodeMap.get(nthNode - 1);
+      prev.next = target.next;
+      target.next = null;
+    }
+  }
+
+  return head;
+};
