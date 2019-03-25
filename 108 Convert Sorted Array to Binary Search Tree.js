89b4a8a76be3ed47e63cf03d613d8df510d15ebc
Jason Wang <wang.jason.010@gmail.com>
2019-03-25 16:10:34 -0700

    sortedarrayBST simple & clean 90% speed

--- 108 Convert Sorted Array to Binary Search Tree.js
+++ 108 Convert Sorted Array to Binary Search Tree.js
@@ -26,3 +26,19 @@ var generate = function(nums, start, end) {
 
   return node;
 };
+
+// 72ms faster than ~90.95% & 37.7MB less than ~41.25%
+var sortedArrayToBST = function(nums) {
+  if (nums.length === 1) return new TreeNode(nums[0]);
+  if (nums.length === 0) return null;
+
+  let mid = Math.floor(nums.length / 2);
+  let root = new TreeNode(nums[mid]);
+  let left = nums.slice(0, mid);
+  let right = nums.slice(mid + 1);
+
+  root.left = sortedArrayToBST(left);
+  root.right = sortedArrayToBST(right);
+
+  return root;
+};
