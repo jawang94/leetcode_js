057eae80aa9f5a71ce5b8d391188de0f2d42f855
Jason Wang <wang.jason.010@gmail.com>
2019-03-25 16:12:27 -0700

    optimized space complexity for sortedArraytoBST

--- 108 Convert Sorted Array to Binary Search Tree.js
+++ 108 Convert Sorted Array to Binary Search Tree.js
@@ -27,18 +27,16 @@ var generate = function(nums, start, end) {
   return node;
 };
 
-// 72ms faster than ~90.95% & 37.7MB less than ~41.25%
+// 72ms faster than ~90.95% & 37.5MB less than ~81.25%
 var sortedArrayToBST = function(nums) {
   if (nums.length === 1) return new TreeNode(nums[0]);
   if (nums.length === 0) return null;
 
-  let mid = Math.floor(nums.length / 2);
+  const mid = Math.floor(nums.length / 2);
   let root = new TreeNode(nums[mid]);
-  let left = nums.slice(0, mid);
-  let right = nums.slice(mid + 1);
 
-  root.left = sortedArrayToBST(left);
-  root.right = sortedArrayToBST(right);
+  root.left = sortedArrayToBST(nums.slice(0, mid));
+  root.right = sortedArrayToBST(nums.slice(mid + 1));
 
   return root;
 };
