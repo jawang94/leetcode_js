cd95606c75ea47375112458d07397a10c68dbf30
Jason Wang <wang.jason.010@gmail.com>
2019-04-02 23:59:08 -0700

    LRU Cache Refactor. Increased speed to 200ms

--- 146 LRU Cache.js
+++ 146 LRU Cache.js
@@ -18,22 +18,22 @@
 // cache.get(3);       // returns 3
 // cache.get(4);       // returns 4
 
-// 228ms Faster than ~62.79%
-/**
- * @param {number} capacity
- */
+// 200ms faster than ~95.00% and 59.5MB less than ~28.57%
 var Node = function(key, value) {
   this.key = key;
   this.value = value;
-  this.next;
   this.prev;
+  this.next;
 };
 
+/**
+ * @param {number} capacity
+ */
 var LRUCache = function(capacity) {
-  this.max = capacity;
-  this.cache = new Map();
-  this.tail = new Node(0, 0);
-  this.head = new Node(0, 0);
+  this.capacity = capacity;
+  this.map = new Map();
+  this.head = new Node(0);
+  this.tail = new Node(0);
   this.head.next = this.tail;
   this.tail.prev = this.head;
 };
@@ -43,8 +43,8 @@ var LRUCache = function(capacity) {
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-  if (this.cache.has(key)) {
-    let node = this.cache.get(key);
+  if (this.map.has(key)) {
+    let node = this.map.get(key);
     this._remove(node);
     this._add(node);
     return node.value;
@@ -58,33 +58,36 @@ LRUCache.prototype.get = function(key) {
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-  if (this.cache.has(key)) {
-    this._remove(this.cache.get(key));
-  }
-  let node = new Node(key, value);
-  this.cache.set(key, node);
-  this._add(node);
-  if (this.cache.size > this.max) {
-    node = this.head.next;
-    this._remove(node);
-    this.cache.delete(node.key);
-  }
-};
+  if (this.map.has(key)) this._remove(this.map.get(key));
 
-LRUCache.prototype._remove = function(node) {
-  node.next.prev = node.prev;
-  node.prev.next = node.next;
+  let node = new Node(key, value);
+  this.map.set(key, node);
+  this._add(node);
+
+  if (this.map.size > this.capacity) {
+    node = this.head.next;
+    this.head.next = node.next;
+    node.prev = this.head;
+    this._remove(node);
+    this.map.delete(node.key);
+  }
 };
 
 LRUCache.prototype._add = function(node) {
-  this.tail.prev.next = node;
   node.next = this.tail;
   node.prev = this.tail.prev;
+  this.tail.prev.next = node;
   this.tail.prev = node;
 };
+
+LRUCache.prototype._remove = function(node) {
+  node.prev.next = node.next;
+  node.next.prev = node.prev;
+};
+
 /**
  * Your LRUCache object will be instantiated and called as such:
- * var obj = Object.create(LRUCache).createNew(capacity)
+ * var obj = new LRUCache(capacity)
  * var param_1 = obj.get(key)
  * obj.put(key,value)
  */
