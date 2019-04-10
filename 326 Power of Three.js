cbd869826d205c96c8276994d164b14f09bba101
Jason Wang <wang.jason.010@gmail.com>
2019-04-10 13:31:01 -0700

    99th percentile power of 3 solution

---
+++ 326 Power of Three.js
@@ -0,0 +1,35 @@
+// Given an integer, write a function to determine if it is a power of three.
+
+// Example 1:
+
+// Input: 27
+// Output: true
+// Example 2:
+
+// Input: 0
+// Output: false
+// Example 3:
+
+// Input: 9
+// Output: true
+// Example 4:
+
+// Input: 45
+// Output: false
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+// 300ms faster than ~99.31% and 48.2MB less than ~30.30%. Could be better memory-wise.
+var isPowerOfThree = function(n) {
+  if (n === 0) return false;
+  let clone = n;
+
+  while (clone >= 3) {
+    clone /= 3;
+  }
+
+  if (clone === 1) return true;
+  return false;
+};
