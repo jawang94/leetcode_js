5ce6a1900b5aab67426acaa8ebf3de257f7aa04e
Jason Wang <wang.jason.010@gmail.com>
2019-04-26 20:44:01 -0700

    path sum tree

--- 112 Path Sum.js
+++ 112 Path Sum.js
@@ -1,29 +1,19 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} root
- * @param {number} sum
- * @return {boolean}
- */
-var hasPathSum = function(root, sum) {
-  if (root === null) {
-    return false;
-  }
+// Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
 
-  if (root.val === sum && root.left === null && root.right === null) {
-    return true;
-  }
+// Note: A leaf is a node with no children.
 
-  return (
-    hasPathSum(root.left, sum - root.val) ||
-    hasPathSum(root.right, sum - root.val)
-  );
-};
+// Example:
+
+// Given the below binary tree and sum = 22,
+
+//       5
+//      / \
+//     4   8
+//    /   / \
+//   11  13  4
+//  /  \      \
+// 7    2      1
+// return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
 
 /**
  * Definition for a binary tree node.
@@ -32,22 +22,16 @@ var hasPathSum = function(root, sum) {
  *     this.left = this.right = null;
  * }
  */
+
 /**
  * @param {TreeNode} root
  * @param {number} sum
  * @return {boolean}
  */
+// 68ms faster than 100% and 37.2MB less than ~48.89%
 var hasPathSum = function(root, sum) {
-  if (root === null) {
-    return false;
-  }
-
-  var left = root.left;
-  var right = root.right;
-
-  if (left === null && right === null) {
-    return root.val === sum;
-  }
-
-  return hasPathSum(left, sum - root.val) || hasPathSum(right, sum - root.val);
+  if (root === null) return false;
+  sum -= root.val;
+  if (root.left === null && root.right === null) return sum === 0;
+  return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
 };
