41d9ae1a2c6b0fbeb7512003f9901b96d5447e2c
Jason Wang <wang.jason.010@gmail.com>
2019-05-07 20:30:53 -0700

    concise DFS construction of binary tree from Inorder and Postorder Traversal

--- 106 Construct Binary Tree from Inorder and Postorder Traversal.js
+++ 106 Construct Binary Tree from Inorder and Postorder Traversal.js
@@ -70,3 +70,20 @@ const search = (inOrder, left, right, value) => {
   }
   return -1;
 };
+
+// Alternate DFS solution ~ same performance as prior solution however more concise
+var buildTree = function(inorder, postorder) {
+  return dfs(inorder.length - 1, 0, inorder.length - 1);
+
+  function dfs(index, startPos, endPos) {
+    if (startPos > endPos) return null;
+
+    var node = new TreeNode(postorder[index]);
+    var pos = inorder.indexOf(postorder[index]);
+
+    node.left = dfs(index - (endPos - pos) - 1, startPos, pos - 1);
+    node.right = dfs(index - 1, pos + 1, endPos);
+
+    return node;
+  }
+};
