605a3f2a782b824ce7893b6dd1f0e4b527463284
Jason Wang <wang.jason.010@gmail.com>
2019-05-09 23:53:57 -0700

    construct binTree w/ preO and inO traversal

--- 105 Construct Binary Tree from Preorder and Inorder Traversal.js
+++ 105 Construct Binary Tree from Preorder and Inorder Traversal.js
@@ -70,3 +70,23 @@ var generate = function(preorder, pl, pr, inorder, il, ir) {
 
   return root;
 };
+
+// Recursive solution 80ms faster than ~78.83% and 36.2MB less than ~74.51%
+var buildTree = function(preorder, inorder) {
+  const recurse = (p1, p2, i1, i2) => {
+    if (p1 > p2 || i1 > i2) return null; // sanity check
+
+    let value = preorder[p1], // get the root value
+      index = inorder.indexOf(value), // get inorder position
+      nLeft = index - i1, // count nodes in left subtree
+      root = new TreeNode(value); // build the root node
+
+    // build the left and right subtrees recursively
+    root.left = recurse(p1 + 1, p1 + nLeft, i1, index - 1);
+    root.right = recurse(p1 + nLeft + 1, p2, index + 1, i2);
+
+    return root;
+  };
+
+  return recurse(0, preorder.length - 1, 0, inorder.length - 1);
+};
